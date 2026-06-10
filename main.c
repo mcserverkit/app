f7bfddb53281789cfdb650a0305d031a7750691e
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-10 14:44:07 -0700

    Make default window size larger

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int main(void)
 #endif
     webview_t w = webview_create(0, NULL);
     webview_set_title(w, "MCServerKit GUI");
-    webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
+    webview_set_size(w, 800, 500, WEBVIEW_HINT_NONE);
     webview_set_html(w, html);
     webview_run(w);
     webview_destroy(w);
