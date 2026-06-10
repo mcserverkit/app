738a573cfb4e0d326c4d3bf5ce3f5d81a51d9b70
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-10 13:08:12 -0700

    Fix #endif in main.c

--- main.c
+++ main.c
@@ -13,11 +13,10 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine,
     (void)hPrevInst;
     (void)lpCmdLine;
     (void)nCmdShow;
-}
 #else
 int main(void)
-#endif
 {
+#endif
     webview_t w = webview_create(0, NULL);
     webview_set_title(w, "Basic Example");
     webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
