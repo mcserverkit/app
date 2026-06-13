0e8c51f6a2ee247a1c0e61f8bd00b5988fdd2af1
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-12 21:35:42 -0700

    Use filesystem to get ui

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,8 @@
 #include "webview/webview.h"
 
 #include <iostream>
+#include <filesystem>
+#include <string>
 
 #ifdef _WIN32
 int WINAPI WinMain(HINSTANCE /*hInst*/, HINSTANCE /*hPrevInst*/,
@@ -15,7 +17,10 @@ int main()
         webview::webview w(false, nullptr);
         w.set_title("MCServerKit GUI");
         w.set_size(480, 320, WEBVIEW_HINT_FIXED);
-        w.set_html(html);
+
+        auto file = std::filesystem::absolute("ui/index.html");
+        auto html = std::string("file:///") + file.generic_string();
+        w.navigate(html);
         w.run();
     }
     catch (const webview::exception &e)
