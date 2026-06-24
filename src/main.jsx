956a0c6a881467907400be8b2985709941ef0bc2
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-24 14:57:32 -0700

    Add navigation bar

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <nav>
+      <button>Servers</button>
+      <button>Plugins</button>
+    </nav>
     <App />
   </StrictMode>,
 );
+g8uuwwe;
